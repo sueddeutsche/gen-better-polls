b12d82dcd5430c7b19532a36ce1a8d61fd1c0ea8
Katharina Brunner <katharina.brunner@sueddeutsche.de>
2017-02-14 09:53:30 +0100

    separate data frames for rolling averages and confidence intervalls

--- tasks/calculations.R
+++ tasks/calculations.R
@@ -1,13 +1,56 @@
+######
+# dataframes
+# df_raw_data: input with raw data from wahlrecht.de
+# df_test_rolling_average: df with rolling average, party and date
+# df_test_standard_error: df with higher and lower confidence intervall limits, in order to show the standard error (alpha = 0.05)
+
+# read raw data 
+df_raw_data <- read.csv("data/data-input-longform.csv", stringsAsFactors = FALSE)
+df_l <- read.csv("data/data-input-longform.csv", stringsAsFactors = FALSE) # old verisobn
+
+###### calculate rolling average
+
+df_rolling_average <- df_raw_data %>%
+  arrange(datum) %>%
+  group_by(datum, partei) %>%
+  mutate(anteil_grouped_by_date = mean(anteil)) %>% 
+  select(datum, partei, anteil_grouped_by_date) %>% 
+  unique() %>% 
+  arrange(datum) %>%
+  group_by(partei) %>%
+  mutate('rolling_average' = round(rollapply(anteil_grouped_by_date, 10, mean, align="right", na.pad = TRUE, na.rm = TRUE),5)) %>% 
+  arrange(desc(datum)) %>% 
+  select(datum, partei, rolling_average)
+
+# save
+write.csv(df_rolling_average, file="data/data-rolling-average.csv", row.names = F, quote = F)
 
 
-
-df_l <- read.csv("data/data-input-longform.csv", stringsAsFactors = FALSE)
+#### calculate limits to show standard error
 
 # se = standard error
 # ci_higher = upper limit of confidence intervall
 # ci_lower = lower limit of confidence intervall
 
+
 number_of_averages <- 10
+
+df_standard_error <- df_raw_data %>% 
+  arrange(datum) %>%
+  group_by(partei) %>%
+  mutate(
+    se = sqrt(anteil * (1-anteil) / befragte),
+    ci_lower = round(anteil - 1.96 * se, 5),
+    ci_higher = round(anteil + 1.96 * se, 5)) %>%
+  arrange(desc(datum)) %>% 
+  select(datum, partei, ci_higher, ci_lower)
+
+write.csv(df_test_standard_error, file="data/data-standarderror.csv", row.names = F, quote = F)
+
+
+
+
+###### old versiobn: one df for everything
 
 # calculate standard error and the confidence intervall
 df_se <- df_l %>%
@@ -23,6 +66,9 @@ df_se <- df_l %>%
 df_se[8:11] <- round(df_se[8:11],5)
 
 write.csv(df_se, file="data/data-transformed.csv", row.names = F, quote = F)
+
+
+
 
 ################
 #### latest average poll/sunday
