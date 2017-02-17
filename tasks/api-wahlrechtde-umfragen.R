f3f1cee1f871cb5e8e3feaac3bdfd36d82f5120b
Schories, Martina <martina.schories@sueddeutsche.de>
2017-02-17 11:09:16 +0100

    getting data via xml-api of wahlrecht.de

---
+++ tasks/api-wahlrechtde-umfragen.R
@@ -0,0 +1,41 @@
+library(xml2)
+
+datum <- vector()
+institut <- vector()
+befragte <- vector()
+partei <- vector()
+anteil <- vector()
+
+# call api
+path <- "http://www.wahlrecht.de/umfragen/bundesweite_gesamt.xml"
+page <- read_xml(path, encoding = "utf-8")
+
+# save data
+wahlrecht_data <- xml_children(xml_root(page))
+for (row in seq_along(wahlrecht_data)){
+  one_poll <- wahlrecht_data[row]
+  parties <- xml_children(xml_child(one_poll,5))
+  for (party in seq_along(parties))
+  {
+    datum <- rbind(datum, xml_text(xml_child(one_poll,3)))
+    institut <- rbind(institut, xml_text(xml_child(one_poll,4)))
+    befragte <- rbind(befragte,  xml_text(xml_child(one_poll,6)))
+    partei <- rbind(partei, xml_name(parties[party]))
+    anteil <- rbind(anteil, xml_text(parties[party]))
+  }
+}
+api_data <- data.frame(datum, institut, befragte, partei, anteil, stringsAsFactors = F)
+
+# refine data
+api_data$befragte <- as.numeric(api_data$befragte)
+api_data$anteil <- as.numeric(api_data$anteil)
+api_data <- filter(api_data, !is.na(api_data$befragte))
+
+refine_party <- function (p){
+  switch (p, cxu = "CDU/CSU", spd = "SPD",afd = "AfD", grn = "Grüne", fpd = "FDP", lnk = "Linke", son = "Sonstige", p)
+}
+api_data$partei <- apply(api_data["partei"], 1, refine_party)
+api_data$anteil <- apply(api_data["anteil"], 1, function(x) x/100)
+
+write.csv(api_data, file="data/data-input-longform.csv", row.names = F, quote = F)
+
