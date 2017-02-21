a1b1c37651eb540d2ff6a9ab9c4a0e5e072b73c9
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2017-02-21 10:31:33 +0100

    Add pkg imports

--- main.R
+++ main.R
@@ -1,4 +1,4 @@
-source("tasks/scrape-wahlrechtde-umfragen.R")
+source("tasks/api-wahlrechtde-umfragen.R")
 source("tasks/calculations.R")
 source("tasks/chart-longterm-polls.R")
 source("tasks/chart-sunday-polls.R")
--- tasks/calculations.R
+++ tasks/calculations.R
@@ -1,3 +1,5 @@
+library(dplyr)
+library(tidyr)
 library(zoo) # rollapply
 
 ######
