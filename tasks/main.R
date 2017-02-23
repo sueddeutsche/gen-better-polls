0a6acce71e9630e1c0d3a0b516ab15841fddae2c
Katharina Brunner <katharina.brunner@sueddeutsche.de>
2017-02-23 11:44:27 +0100

    mist :)

---
+++ tasks/main.R
@@ -0,0 +1,10 @@
+source("tasks/config.R")
+<<<<<<< HEAD
+source("tasks/scrape-wahlrechtde-umfragen.R")
+=======
+source("tasks/api-wahlrechtde-umfragen.R")
+>>>>>>> 8f5ae3a4bb59c685d2a295b7f8e894fe78d1a5c3
+source("tasks/calculations.R")
+source("tasks/chart-longterm-polls.R")
+source("tasks/chart-sunday-polls.R")
+system("node tasks/buildReadme")
