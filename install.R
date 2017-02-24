4d2306e80809a31333097f04529abf7ac1661335
Sascha Goldhofer <sascha.goldhofer@sueddeutsche.de>
2017-02-24 11:57:18 +0100

    Disable additional installs

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 install.packages(c("packrat"))
 library(packrat)
 packrat::init()
-install.packages(c("ggplot2", "purrr", "selectr", "xml2", "dplyr", "magrittr", "zoo", "directlabels"))
+# install.packages(c("ggplot2", "purrr", "selectr", "xml2", "dplyr", "magrittr", "zoo", "directlabels"))
 packrat::snapshot()
