7dbb61bceb1fbb05dc1067f3965d35edc065a313
Katharina Brunner <katharina.brunner@sueddeutsche.de>
2017-03-20 14:49:08 +0100

    Add cleaner script

--- tasks/calculations_gauss.R
+++ tasks/calculations_gauss.R
@@ -1,26 +1,40 @@
+######
+# dataframes
+# df_raw_data: input with raw data from wahlrecht.de
+# df_test_rolling_average: df with rolling average, party and date
+# df_test_standard_error: df with higher and lower confidence intervall limits, in order to show the standard error (alpha = 0.05)
+
+# read raw data 
 df_raw_data <- read.csv("data/data-input-longform.csv", stringsAsFactors = FALSE)
+df_l <- read.csv("data/data-input-longform.csv", stringsAsFactors = FALSE) # old version
 
-
-# exclude all rows with NA in anteil, problem e.g. 2017-02-02 with INSA
 df_raw_data <- df_raw_data %>% 
-filter(partei %in% c("CDU/CSU", "SPD", "Grüne", "Linke", "AfD", "FDP"), !is.na(anteil))
-# filter(partei %in% c("CDU/CSU", "SPD"), )
+  filter(partei %in% c("CDU/CSU", "SPD", "Grüne", "Linke", "AfD", "FDP"), !is.na(anteil))
 
-  
-
-# get rolling average over last 10 entries
 df_rolling_average <- df_raw_data %>%
   arrange(datum) %>%
   select(datum, partei, anteil, befragte) %>% 
   unique() %>% 
   arrange(datum) %>%
   group_by(partei) %>%
-  mutate('rolling_average' = round(rollapply(anteil, 10, mean, align="right", fill = NA),5)) %>% 
+  mutate(rolling_average = round(rollapply(anteil, 10, mean, align="right", fill=NA, na.rm = TRUE),5)) %>% 
   arrange(desc(datum)) %>% 
   select(datum, partei, rolling_average)
 
+# save
+write.csv(df_rolling_average, file="data/data-rolling-average.csv", row.names = F, quote = F)
 
-# get standard error, delta and confidence intervall limits from raw input data
+
+#### calculate limits to show standard error
+
+# se = standard error
+# delta = standart error times 1.96
+# ci_higher = upper limit of confidence intervall
+# ci_lower = lower limit of confidence intervall
+
+
+number_of_averages <- 10
+
 df_standard_error <- df_raw_data %>% 
   arrange(datum) %>%
   select(datum,partei,anteil,befragte) %>%
@@ -29,14 +43,24 @@ df_standard_error <- df_raw_data %>%
   group_by(partei) %>%
   mutate(
     se = sqrt(anteil * (1-anteil) / befragte),
-    delta = 1.96 * se, #NEU
+    delta = round(1.96 * se,5), #NEU
     ci_lower = round(anteil - 1.96 * se, 5),
     ci_higher = round(anteil + 1.96 * se, 5)) %>%
   arrange(desc(datum)) %>% 
   select(datum, partei, delta) #NEU: delta wird exportiert
 
+write.csv(df_standard_error, file="data/data-standarderror.csv", row.names = F, quote = F)
 
-gauss <- function(x, na.rm = T){
+df_datum <- df_standard_error$datum %>% unique()
+df_datum <- as.data.frame(df_datum)
+names(df_datum) <- c("datum")
+
+
+
+
+### Fehlerfortpflanzung
+
+error_gauss <- function(x){
   i <- 1
   l <- length(x)
   qs <- 0 #quadratsumme
@@ -44,28 +68,39 @@ gauss <- function(x, na.rm = T){
     qs <- qs + x[i]*x[i]  
   }
   gaussfehler <- sqrt(1/l) * sqrt(qs)
+  #  gaussfehler <- 1/l * sqrt(qs)
   gaussfehler
 }
 
-df_rolling_gauss <- df_standard_error %>%
+error_linear <- function(x){
+  i <- 1
+  l <- length(x)
+  sum <- 0
+  for (i in 1:l){
+    sum <- sum + x[i]
+  }
+  fehlersumme <- 1 / l * sum
+  fehlersumme
+}
+
+df_rolling_error <- df_standard_error %>%
   select(datum, partei, delta) %>%
   unique() %>%
   arrange(datum) %>%
   group_by(partei) %>%
-  mutate(rolling_gauss = rollapply(delta, 10, gauss, align = "right", fill = NA)) %>%
+  mutate(rolling_error = round(rollapply(delta,10,error_linear, align = "right", fill= NA),5)) %>%
   arrange(desc(datum))
 
+write.csv(df_rolling_error, file="data/data-rolling_error.csv", row.names = FALSE, quote = FALSE)
 
-
-
-
-  
-# join rolling_average and rolling_gauss_error
-df_rolling_average_and_gauss <- left_join(df_rolling_average, df_rolling_gauss, by = c("datum", "partei")) %>%
+df_rolling_average_and_error <- left_join(df_rolling_average, df_rolling_error, by = c("datum", "partei"))
+df_rolling_average_and_error %>% names
+df_alles_rollt <- 
+  df_rolling_average_and_error %>%
   ungroup() %>%
   arrange(datum) %>% 
-  mutate(rolling_upper_ci = rolling_average + rolling_gauss) %>%
-  mutate(rolling_lower_ci = rolling_average - rolling_gauss) %>% 
-  arrange(desc(datum))
+  mutate(rolling_upper_ci = rolling_average + rolling_error) %>%
+  mutate(rolling_lower_ci = rolling_average - rolling_error)
 
-write.csv(df_rolling_average_and_gauss, file="data/data-rolling-average-gauss.csv", row.names = F, quote = F)
+write.csv(df_rolling_average_and_error, file="data/data-rolling_average_and_error.csv", row.names = FALSE, quote = FALSE)
+
--- tasks/main.R
+++ tasks/main.R
@@ -1,10 +1,11 @@
 source("tasks/config.R")
-<<<<<<< HEAD
+
 source("tasks/scrape-wahlrechtde-umfragen.R")
-=======
-source("tasks/api-wahlrechtde-umfragen.R")
->>>>>>> 8f5ae3a4bb59c685d2a295b7f8e894fe78d1a5c3
-source("tasks/calculations.R")
+
+#source("tasks/api-wahlrechtde-umfragen.R")
+
+#source("tasks/calculations.R")
+source("tasks/calculations_gauss.R")
 source("tasks/chart-longterm-polls.R")
-source("tasks/chart-sunday-polls.R")
+source("tasks/chart-sunday-polls-gauss.R")
 system("node tasks/buildReadme")
