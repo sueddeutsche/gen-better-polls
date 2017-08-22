1ef198eb237cfb3a8bb67a79c566e290a274950d
Schories, Martina <martina.schories@sueddeutsche.de>
2017-08-22 17:21:49 +0200

    Added hidden characters for labels

--- tasks/chart-longterm-polls-special.R
+++ tasks/chart-longterm-polls-special.R
@@ -10,13 +10,16 @@ df_rolling_average_and_error_party <- unique(df_rolling_average_and_error$partei
 
 
 latest_values <- arrange(df_rolling_average_and_error, desc(datum)) %>% filter(datum == datum[1])
+hidden_chars <- c("\U200C","\u200D","\u200E","\u200F","\U200C","\u200D")
+latest_values <- arrange(latest_values, desc(rolling_average))
+latest_values <- cbind(latest_values, hidden_chars)
 startDatum <- "2015-06-01"
 df_rolling_average_and_error <- filter(df_rolling_average_and_error, datum > startDatum)
 
 # andere Namen für die Linien als das Standardlabel
 get_label_value <- function (partei){
   index = match(partei, latest_values$partei)
-  label = paste0(round(latest_values$ci_lower[index]*100, digits = 0), "-", round(latest_values$ci_higher[index]*100, digits = 0), "%")
+  label = paste0(round(latest_values$ci_lower[index]*100, digits = 0), "-", round(latest_values$ci_higher[index]*100, digits = 0), "%",latest_values$hidden_chars[index] )
   label = as.character(label)
 }
 
@@ -36,16 +39,19 @@ do_special_party <- function(party){
   article_chart <- basechart + sztheme_lines +
     scale_x_date(date_labels = "%B %y", limits = as.Date(c(startDatum, NA)), expand = c(0, 0))
   mobile_chart <- basechart + sztheme_lines + sztheme_lines_mobile  +
-    scale_x_date(date_labels = "%m.%y", limits = as.Date(c(startDatum, NA)), expand = c(0, 0))
+    scale_x_date(date_labels = "%m/%y", limits = as.Date(c(startDatum, NA)), expand = c(0, 0))
   
   article_chart <- ggplotGrob(article_chart)
   article_chart$layout$clip[article_chart$layout$name == "panel"] <- "off"
   mobile_chart <- ggplotGrob(mobile_chart)
   mobile_chart$layout$clip[mobile_chart$layout$name == "panel"] <- "off"
   
-  ggsave(file=paste0("data/assets/longterm-poll-article-",party,".png"), plot=article_chart, dpi = 144, units = "in", width = 8.89, height = 5)
-  # ggsave(file="data/assets/longterm-poll-hp.png", plot=article_chart, dpi = 144, units = "in", width = 7.78, height = 4.38)
-  ggsave(file=paste0("data/assets/longterm-poll-mobile-", party,".png"), plot=mobile_chart, dpi = 144, units = "in", width = 4.45, height = 3.33)
+  ggsave(file=paste0("data/assets/longterm-poll-article-cdu.png"), plot=article_chart, dpi = 144, units = "in", width = 8.89, height = 5)
+  ggsave(file=paste0("data/assets/longterm-poll-mobile-cdu.png"), plot=mobile_chart, dpi = 144, units = "in", width = 4.45, height = 3.33)
+  
+  # ggsave(file=paste0("data/assets/longterm-poll-article-",party,".png"), plot=article_chart, dpi = 144, units = "in", width = 8.89, height = 5)
+  # # ggsave(file="data/assets/longterm-poll-hp.png", plot=article_chart, dpi = 144, units = "in", width = 7.78, height = 4.38)
+  # ggsave(file=paste0("data/assets/longterm-poll-mobile-", party,".png"), plot=mobile_chart, dpi = 144, units = "in", width = 4.45, height = 3.33)
 }
 
-do_special_party("Grüne")
+do_special_party("CDU/CSU")
